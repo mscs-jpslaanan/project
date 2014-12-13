d48adc7c96949cb4ea06ca0291428531431299ce
mscs-jpslaanan <john_patrick_laanan@yahoo.com>
2014-12-13 13:00:38 +0800

    Current date is displayed in HTML; Current date unit test

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'todo.views.home', name='home'),
+    #url(r'^$', 'todo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^home/$', 'todo.views.home_page', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
