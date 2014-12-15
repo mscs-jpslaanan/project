edf756959b3d47433ee8bc2714fddfd9c0f41fad
mscs-jpslaanan <john_patrick_laanan@yahoo.com>
2014-12-15 08:40:30 +0800

    Created urls.py in the todo app; configured urls.py in todo and project

--- project/urls.py
+++ project/urls.py
@@ -5,8 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'todo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^home/$', 'todo.views.home_page', name='home'),
-    url(r'^home/tick_done/(?P<todoID>\d+)/$', 'todo.views.tick_done', name='done'),
-    url(r'^home/tick_cancel/(?P<todoID>\d+)/$', 'todo.views.tick_cancel', name='cancel'),
+    (r'^todo/', include('todo.urls')),
     #url(r'^admin/', include(admin.site.urls)),
 )
